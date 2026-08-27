e3da3c1aece080c3922f483b872a73f1fc9f6dcc
BudhvinHewage <budhvin.hewage@outlook.com>
2026-08-26 20:12:59 -0400

    added memory and a top level cpu to test using a testbench file

---
+++ Semi RISC CPU Verilog.srcs/sources_1/new/cpu_top_test.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+
+module cpu_top_test(
+    input  cpu_clk, mem_clk, rst,
+    output [31:0] out_a, out_b,
+    output out_c, out_z,
+    output [31:0] out_ir, out_pc,
+    output [2:0]  t_info
+);
+    wire [31:0] cpu_to_mem, mem_to_cpu, addr_from_cpu;
+    wire wen_mem_dbg, en_mem_dbg;
+
+    instr_mem imem (
+        .clock(mem_clk),
+        .address(addr_from_cpu[5:0]),
+        .data(cpu_to_mem),
+        .wren(1'b0),          
+        .q(mem_to_cpu)
+    );
+
+    cpu1 cpu (
+        .clk(cpu_clk), .mem_clk(mem_clk), .rst(rst),
+        .data_in(mem_to_cpu), .data_out(cpu_to_mem),
+        .addr_out(addr_from_cpu),
+        .dout_a(out_a), .dout_b(out_b),
+        .dout_c(out_c), .dout_z(out_z),
+        .dout_ir(out_ir), .dout_pc(out_pc),
+        .out_t(t_info),
+        .wen_mem(wen_mem_dbg), .en_mem(en_mem_dbg)
+    );
+endmodule
